5cb484f4278e73d02927bd5863d4da360951f5ac
cplus-project <cplus-project@qq.com>
2015-02-22 21:08:51 +0800

    plan to implement the ast for c+ language

---
+++ src/ast.h
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2015 JiKai. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ *
+ *     The ast.h and ast.c implement the abstract
+ * syntax tree of the c+ language.
+ **/
+
+#ifndef CPLUS_AST_H
+#define CPLIS_AST_H
+
+#include "common.h"
+
+#endif
